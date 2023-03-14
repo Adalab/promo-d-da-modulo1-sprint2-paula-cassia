7a7567b13e166218ff34218c171126cd56492532
cassia-elx <cassiadafoe@gmail.com>
2023-03-14 09:01:32 +0100

    creados archivos ejercicios 10

---
+++ SQL/ejercicios-SQL-10.sql
@@ -0,0 +1,34 @@
+/* LECCION 10: Consultas en multiples tablas 2 */
+
+/* EJERCICIO 1  Qué empresas tenemos en la BBDD Northwind:
+Lo primero que queremos hacer es obtener una consulta SQL que nos devuelva el nombre de 
+todas las empresas cliente, los ID de sus pedidos y las fechas. */
+
+
+
+/* EJERCICIO 2  Pedidos por cliente de UK:
+Desde la oficina de Reino Unido (UK) nos solicitan información acerca del número de pedidos que 
+ha realizado cada cliente del propio Reino Unido de cara a conocerlos mejor y poder adaptarse al mercado actual. 
+Especificamente nos piden el nombre de cada compañía cliente junto con el número de pedidos. */
+
+
+
+/* EJERCICIO 3  Empresas de UK y sus pedidos:
+También nos han pedido que obtengamos todos los nombres de las empresas cliente de Reino Unido 
+(tengan pedidos o no) junto con los ID de todos los pedidos que han realizado, el nombre de contacto 
+de cada empresa y la fecha del pedido. */
+
+
+
+/* EJERCICIO 4  Empleadas que sean de la misma ciudad:
+Ejercicio de SELF JOIN: Desde recursos humanos nos piden realizar una consulta que muestre por pantalla 
+los datos de todas las empleadas y sus supervisoras. Concretamente nos piden: la ubicación, nombre, y apellido 
+tanto de las empleadas como de las jefas. Investiga el resultado, ¿sabes decir quién es el director? */
+
+
+
+/* EJERCICIO 5  BONUS: FULL OUTER JOIN Pedidos y empresas con pedidos asociados o no:
+Selecciona todos los pedidos, tengan empresa asociada o no, y todas las empresas tengan pedidos asociados o no. 
+Muestra el ID del pedido, el nombre de la empresa y la fecha del pedido (si existe). */
+
+
