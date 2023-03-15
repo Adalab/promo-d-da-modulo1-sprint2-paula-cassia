72d922b755a96ac81e490440d7a236f2d2c52d43
Paula <paulavelascosuarez6@gmail.comgit config --global user.email paulavelascosuarez6@gmail.com>
2023-03-15 11:43:22 +0000

    ejercicio 11 sql terminado

--- SQL/ejercicios-SQL-11.sql
+++ SQL/ejercicios-SQL-11.sql
@@ -1,41 +1,41 @@
 /* LECCION 11: Consultas en Multiples Tablas 3 */
 
+USE northwind;
+
 -- EJERCICIO 1
 
-SELECT contact_last_name AS Apellidos
+SELECT city AS City, company_name AS CompanyName, contact_name AS ContacName, "Customers" AS Relationship
 FROM customers
-UNION
-SELECT last_name AS Apellidos
-FROM employees;
+UNION 
+SELECT city, company_name, contact_name, "Suppliers"
+FROM suppliers;
 
 -- EJERCICIO 2
 
-SELECT contact_first_name AS Nombre, contact_last_name AS Apellidos
-FROM customers
-UNION
-SELECT first_name AS Nombre, last_name AS Apellidos
-FROM employees;
+SELECT *
+FROM orders
+WHERE employee_id IN (
+SELECT employee_id 
+FROM employees
+WHERE first_name = "Nancy" 
+AND last_name = "Davolio");
 
 -- EJERCICIO 3
 
-SELECT contact_first_name AS Nombre, contact_last_name AS Apellidos
+SELECT company_name AS CompanyName, country AS Country
 FROM customers
-UNION ALL
-SELECT first_name AS Nombre, last_name AS Apellidos
-FROM employees;
+WHERE customer_id NOT IN (
+SELECT customer_id
+FROM orders
+WHERE year(order_date) = 1997);
 
 -- EJERCICIO 4
 
-SELECT employee_number AS NumeroEmpleado, first_name AS NombreEmpleado, last_name AS ApellidoEmpleado
-FROM employees
-WHERE employee_number IN (
-	SELECT sales_rep_employee_number
-    FROM customers);
+SELECT *
+FROM orders
+WHERE order_id IN (
+SELECT order_details.order_id 
+FROM order_details INNER JOIN products
+ON order_details.product_id = products.product_id
+WHERE products.product_name = "Konbu");
 
--- EJERCICIO 5
-
-SELECT city AS Ciudad, customer_name AS NombredelaEmpresa
-FROM customers
-WHERE city NOT IN (
-	SELECT city
-    FROM offices);
