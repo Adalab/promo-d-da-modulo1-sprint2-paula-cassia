4473ea17f3ca8fbb6d5544fc3d778aebe243064d
cassia-elx <cassiadafoe@gmail.com>
2023-03-22 08:55:53 +0100

    creacion archivos ejercicios 14

---
+++ SQL/ejercicios-SQL-14.sql
@@ -0,0 +1,25 @@
+/* LECCION 14: CTEs */
+
+/* Ejercicio 1: Extraer en una CTE todos los nombres de las compañias y los id de los clientes.
+Para empezar nos han mandado hacer una CTE muy sencilla el id del cliente y el nombre de la compañia de la tabla Customers. */
+
+
+/* Ejercicio 2: Selecciona solo los de que vengan de "Germany"
+Ampliemos un poco la query anterior. En este caso, queremos un resultado similar al anterior, pero solo 
+queremos los que pertezcan a "Germany". */
+
+
+
+/* Ejercicio 3: Extraed el id de las facturas y su fecha de cada cliente.
+En este caso queremos extraer todas las facturas que se han emitido a un cliente, su fecha la compañia a la que pertenece.
+📌 NOTA En este caso tendremos columnas con elementos repetidos(CustomerID, y Company Name).*/
+
+
+/* Ejercicio 4: Contad el número de facturas por cliente
+Mejoremos la query anterior. En este caso queremos saber el número de facturas emitidas por cada cliente. */
+
+
+/* Ejercicio 5: Cuál la cantidad media pedida de todos los productos ProductID.
+Necesitaréis extraer la suma de las cantidades por cada producto y calcular la media. */
+
+
